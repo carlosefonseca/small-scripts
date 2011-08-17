1b0c6f634efdbd715853d8777e2c687ddcf098d7
Carlos Fonseca <carlosefonseca@gmail.com>
2011-08-17 23:10:36 +0100

    added

---
+++ ohlife2dayone.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datetime import datetime
+import os, sys
+
+
+
+def sendToDayOne(date, body):
+    command = 'echo "'+body.strip()+'" | /Applications/Day One.app/Contents/MacOS/dayone -d="'+date.strip()+'" new'
+    print command
+    print u"—————————————————————————————————————————————————————————————————"
+    os.system(command)
+    
+
+
+f = open("ohlife.txt")
+
+date = "";
+body = "";
+for line in f:
+    try:
+        datetime.strptime(line,"%Y-%m-%d\n")
+        if date != "":
+            sendToDayOne(date, body)
+        date = line
+        body = ""
+    except ValueError:
+        body += line        
+sendToDayOne(date, body)
