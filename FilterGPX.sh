86d75b65057c42068008e636b5fa91ce2505b8cf
Carlos Fonseca <carlosefonseca@gmail.com>
2011-08-30 17:23:00 +0100

    Downloads the gpx file if a file is not specified

--- FilterGPX.sh
+++ FilterGPX.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-perl -pe 's/\n//;s/<wpt/\n<wpt/;' $1 | perl -ne 'my $hilat=39.3;my $lowlat=38.68;if(! m/wpt/) {print;}else{m/lat="(\d+\.\d+)/;if($1>$lowlat and $1<$hilat){print;}}' ; echo ""
+if [[ $# -eq 0 ]]; then
+	curl -o tmp.gpx "http://against.org/munzee/generate_gpx.php?whatever-"
+	GPX=tmp.gpx
+	perl -pe 's/\n//;s/<wpt/\n<wpt/;' $GPX | perl -ne 'my $hilat=39.3;my $lowlat=38.68;if(! m/wpt/) {print;}else{m/lat="(\d+\.\d+)/;if($1>$lowlat and $1<$hilat){print;}}' > ~/Dropbox/munzee-centro.gpx
+	~/Code/growl.sh -nosticky "Munzee GPX updated."
+	rm tmp.gpx
+else 
+	GPX=$1
+	perl -pe 's/\n//;s/<wpt/\n<wpt/;' $GPX | perl -ne 'my $hilat=39.3;my $lowlat=38.68;if(! m/wpt/) {print;}else{m/lat="(\d+\.\d+)/;if($1>$lowlat and $1<$hilat){print;}}' ; echo ""
+fi
+
