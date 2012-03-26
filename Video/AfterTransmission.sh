d494599d6512292c4b412341b5643df198bc59f3
Carlos Fonseca <carlosefonseca@gmail.com>
2012-03-26 22:06:50 +0100

    Moved the "move mp4" script to autoConvert and fixed a folder name

--- Video/AfterTransmission.sh
+++ Video/AfterTransmission.sh
@@ -7,12 +7,5 @@ echo "$TR_TORRENT_DIR/$TR_TORRENT_NAME" > ~/Desktop/log
 
 F="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
 
-EXT=`echo $F | sed -E 's/^.*\.([^.*])/\1/'`
-if [ $EXT == "m4v" ]; then
-    cp "$F" "/Users/carlos/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/"
-elif [ $EXT == "mp4" ]; then
-    F2=`echo $TR_TORRENT_NAME | sed 's/mp4/m4v/'`
-    cp "$F" "/Users/carlos/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/$F2"
-else
-    /Users/carlos/Dropbox/Code/small-scripts/Video/autoConvert.sh "$TR_TORRENT_DIR/$TR_TORRENT_NAME" >>~/Desktop/log 2>&1
-fi
+
+/Users/carlos/Dropbox/Code/small-scripts/Video/autoConvert.sh "$TR_TORRENT_DIR/$TR_TORRENT_NAME" >>~/Desktop/log 2>&1
--- Video/autoConvert.sh
+++ Video/autoConvert.sh
@@ -6,7 +6,7 @@ RENAME="/Users/carlos/Code/tvrenamer.pl --scheme=XxYY --dubious --include-series
 HandBrakeCLI="/Users/carlos/Dropbox/Code/small-scripts/Video/HandBrakeCLI"
 
 #PATHS
-iTUNES="/Users/carlos/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/"
+iTUNES="/Users/carlos/Dropbox/iTunes/iTunes Media/Automatically Add to iTunes.localized/"
 CONVERT="/Users/carlos/Movies/MP4"
 
 DIR=`dirname "$1"`
@@ -23,13 +23,29 @@ fi
 INOD=`stat -f %i "$1"`
 
 s=`echo $1 | perl -lane '$v="@F[0..20]"; $v=~m/(\/([^\/]+\/)+)/g; print \$1'`
+
+
 /Users/carlos/Code/tvrenamer.pl --scheme=XxYY --dubious --include-series --unattended --chdir="$s"
 
 NEWFILE=`find "$DIR" -inum $INOD -print`
 F=$NEWFILE
+
+NAME=`basename "$F"`
+EXT=`echo $F | sed -E 's/^.*\.([^.*])/\1/'`
+echo $EXT
+
+if [ $EXT == "mp4" ]; then
+	echo "no need to convert"
+	cp -v "$F" "${F}0"
+    mv "${F}0" "/Users/carlos/Dropbox/iTunes/iTunes Media/Automatically Add to iTunes.localized/$NAME"
+    #echo mv "\"${F}0\" \"/Users/carlos/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/$NAME\""
+else
+
 
 FILE=`basename -s .avi "$F"`.m4v
 OUT="$CONVERT/$FILE"
 echo "Converting \"$F\" to \"$OUT\""
 nice -n +100 $HandBrakeCLI -i "$F" -o "$OUT" --preset="iPhone & iPod Touch"
 mv "$OUT" "$iTUNES"
+
+fi
